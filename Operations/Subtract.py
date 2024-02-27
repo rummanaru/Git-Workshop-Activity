68766dc46f6ca60784d67cda0136bfa18db9013d
Allison Yeh <yehas1@uci.edu>
2024-02-26 22:09:07 -0800

    Removed variable declaration

--- Operations/Subtract.py
+++ Operations/Subtract.py
@@ -1,6 +1,4 @@
 def subtract(operand_1, operand_2):
     # TODO: Calculate the difference of the 2 operands and return it
-    double float
-    
-    value = operand_1 - operand_2
-    return value
+    return operand_1 - operand_2
+
