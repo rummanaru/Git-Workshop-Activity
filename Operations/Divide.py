26197f67a0b7dc69980c66d31f6fc633340b3071
Chenglong Cai <chengloc@uci.edu>
2024-02-26 17:24:22 -0800

    test2

--- Operations/Divide.py
+++ Operations/Divide.py
@@ -1,3 +1,3 @@
 def divide(operand_1, operand_2):
     # TODO: Calculate the quotient of the 2 operands and return it
-    pass
+    return operand_1/operand_2
--- Operations/Multiply.py
+++ Operations/Multiply.py
@@ -1,3 +1,3 @@
 def multiply(operand_1, operand_2):
     # TODO: Calculate the product of the 2 operands and return it
-    pass
+    return operand_1*operand_2
